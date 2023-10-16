a9a46436368c585d8548a1a84a9b86d36816e7d0
joshuakalungi <joshuakalungi101@gmail.com>
2023-10-16 23:37:58 +0300

    setup

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 import os
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from config import DevConfig
+from models import Recipe
+from etxs import db
+
 
 app = Flask(__name__)
 
 app.config.from_object(DevConfig)
+
+db.init_app(app)
+
 api = Api(app, doc="/docs")
 
 @api.route('/hello')
@@ -13,7 +19,17 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+# model (serializer)
+recipe_model = api.model("Recipe",{
+    "id": fields.Integer(),
+    "title": fields.String(),
+    "description": fields.String()
+}
+)
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, Recipe=Recipe)
 
 if __name__ == '__main__':
     app.run()
--- backend/models.py
+++ backend/models.py
@@ -12,6 +12,24 @@ class Recipe:
 
 
 class Recipe(db.Model):
-    id = db.Column(db.Integer(),primary_key=True),
-    title = db.Column(db.String(), nullable=False)
-    description = db.Column(db.Text(), nullable=False)
+    id = db.Column(db.Integer(),primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text(200), nullable=False)
+
+
+    def __repr__(self):
+        return f"<Recipe{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self,title,description):
+        self.title = title
+        self.description = description
+        db.session.add()
+        db.session.commit()
